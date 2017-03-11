f5169359c1053f2655698e6215c94cff5307f2d1
Moises <molimpio@hotmail.com.br>
2017-03-11 17:11:24 -0300

    filtrando a lista de fotos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtrar pelo título" autofocus />
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
@@ -24,6 +25,19 @@ export default {
     return {
       titulo: 'Alurapic',
       fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
     }
   },
 
@@ -59,6 +73,11 @@ export default {
   }
 
   .imagem-responsiva {
+    width: 100%;
+  }
+
+  .filtro {
+    display: block;
     width: 100%;
   }
 
--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="painel">
     <h2 class="painel-titulo">{{ titulo }}</h2>
-    <div class="painel-conteudo">      
-    </div>
+    <slot class="painel-conteudo">
+    </slot>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
    .painel {
     padding: 0 auto;
     border: solid 2px grey;
@@ -33,5 +33,9 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
   }
 </style>
