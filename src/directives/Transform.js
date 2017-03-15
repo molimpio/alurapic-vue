3685a37cc9da552356420cd98e05fe3902690ca4
Moises <molimpio@hotmail.com.br>
2017-03-15 20:55:51 -0300

    rotacionando a imagem com criacao de diretiva

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -5,8 +5,12 @@ Vue.directive('meu-transform', {
 
   bind(el, binding, vnode){
 
-      console.log('diretiva associada');
-      console.log(el);
+      let current = 0;
+
+      el.addEventListener('dblclick', function(){
+        current+=90;
+        el.style.transform = `rotate(${current}deg)`;
+      });
   }
 
 });
