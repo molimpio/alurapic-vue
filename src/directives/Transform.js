01c89255eaf86087deeb223bb74b6d7a730a9195
Moises <molimpio@hotmail.com.br>
2017-03-15 21:05:01 -0300

    passando parametros em formato obj para diretiva

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -3,13 +3,27 @@ import Vue from 'vue';
 //para usar usa v-meu-transform
 Vue.directive('meu-transform', {
 
+  // el => elemento DOM
+  // binding => pega valores passados pela diretiva
+  // vnode =>
   bind(el, binding, vnode){
 
       let current = 0;
 
       el.addEventListener('dblclick', function(){
-        current+=90;
+
+        let incremento = 90;
+        let animate = false;
+
+        if(binding.value) {
+          incremento = binding.value.incremento;
+          animate = binding.value.animate;
+        }
+
+        current+=incremento;
         el.style.transform = `rotate(${current}deg)`;
+
+        if(animate) el.style.transition = 'transform 0.5s';
       });
   }
 
