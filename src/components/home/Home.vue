8bb80c83bf42fef42d501da8d3269d6588a3072f
Moises <molimpio@hotmail.com.br>
2017-03-19 09:52:34 -0300

    trabaljandocom resource

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -57,8 +57,9 @@ export default {
 
   methods: {
     remove(foto){
-      this.$http
-      .delete(`v1/fotos/${foto._id}`)
+
+      this.resource
+      .delete({id: foto._id})
       .then( ()=> {
         let indice = this.fotos.indexOf(foto);
         this.fotos.splice(indice,1);
@@ -76,8 +77,12 @@ export default {
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));*/
 
-    this.$http.get('v1/fotos')
+    this.resource = this.$resource('v1/fotos{/id}');
+
+    this.resource
+      .query()
       .then(res => this.fotos = res.body, err => console.log(err));
+
   }
 
 }
