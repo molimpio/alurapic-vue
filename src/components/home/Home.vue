b01e32d015426f602e2d7e8b595c8303a7df1b1d
Moises <molimpio@hotmail.com.br>
2017-03-19 09:30:47 -0300

    removendo foto

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 class="centralizado">{{ titulo }}</h1>
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
     <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Filtrar pelo título" autofocus />
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
@@ -37,7 +38,8 @@ export default {
     return {
       titulo: 'Alurapic',
       fotos: [],
-      filtro: ''
+      filtro: '',
+      mensagem: ''
     }
   },
 
@@ -55,7 +57,13 @@ export default {
 
   methods: {
     remove(foto){
-      alert('Remover foto '+foto.titulo);
+      this.$http
+      .delete(`http://localhost:3000/v1/fotos/${foto._id}`)
+      .then( ()=> this.mensagem = 'Foto removida com sucesso', err => {
+        console.log(err);
+        this.mensagem = 'Não foi possível remover a foto';
+      });
+
     }
   },
 
