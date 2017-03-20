ea7076efb82e8c05fad4ff88e2c53e7682865e6b
Moises <molimpio@hotmail.com.br>
2017-03-20 05:15:32 -0300

    fazendo a rota para alteracao

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -8,6 +8,9 @@
         <meu-painel :titulo="foto.titulo">
 
           <imagem-responsiva v-meu-transform:scale.animate="1.1" :url="foto.url" :titulo="foto.titulo"/>
+          <router-link :to="{ name: 'altera', params: { id: foto._id } }">
+            <meu-botao tipo="button" rotulo="ALTERAR" />
+          </router-link>
           <meu-botao
             tipo="button"
             rotulo="Remover"
