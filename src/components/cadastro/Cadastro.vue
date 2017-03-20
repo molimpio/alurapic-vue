b3a954cb193e7c07b76ee69329aa4865b7ecf802
Moises <molimpio@hotmail.com.br>
2017-03-20 05:33:35 -0300

    alterando a foto e redirecionando usuario para home

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -4,6 +4,8 @@
     <h1 class="centralizado">Cadastro</h1>
     <h2 class="centralizado"></h2>
 
+    <h2 v-if="foto._id" class="centralizado">Alterando</h2>
+    <h2 v-else class="centralizado">Incluindo</h2>
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
@@ -58,7 +60,10 @@ export default {
 
           this.service
             .cadastra(this.foto)
-            .then( ()=> this.foto = new Foto(), err => console.log(err));
+            .then( ()=> {
+                if(this.id) this.$router.push({ name: 'home'});
+                this.foto = new Foto();
+              }, err => console.log(err));
       }
   },
 
